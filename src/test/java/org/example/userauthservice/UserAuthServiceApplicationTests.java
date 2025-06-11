d3910cda69df7abda67ebe866c0ae179290951fe
mohan2k178 <akshara2k17@gmail.com>
2025-06-11 01:49:24 -0400

    Implementing oAuth2

--- src/test/java/org/example/userauthservice/UserAuthServiceApplicationTests.java
+++ src/test/java/org/example/userauthservice/UserAuthServiceApplicationTests.java
@@ -1,13 +1,47 @@
 package org.example.userauthservice;
 
+import org.example.userauthservice.security.repositories.JpaRegisteredClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserAuthServiceApplicationTests {
+    @Autowired
+    private JpaRegisteredClientRepository registeredClientRepository;
 
     @Test
     void contextLoads() {
     }
+
+//    @Test
+//    public void testCase(){
+//
+//            RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+//				.clientId("oidc-client")
+//				.clientSecret("{noop}secret")
+//				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//				.redirectUri("https://oauth.pstmn.io/v1/callback")
+//				.postLogoutRedirectUri("http://127.0.0.1:8080/")
+//				.scope("ADMIN")
+//				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+//				.build();
+//
+//		registeredClientRepository.save(oidcClient);
+//
+//
+//    }
 
 }
